82ef15851eaac2b0f9b6cccb5ba5f46c11947702
YuraPaisanidi <yura.paisanidi@gmail.com>
2023-06-23 16:34:20 +0300

    first commit

---
+++ src/pages/index/app.js
@@ -0,0 +1,24 @@
+import gsap from "gsap"
+import { ScrollTrigger } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger)
+
+// gsap.set('#elem2', { y: '100%', opacity: 0 })
+
+const stageTl = gsap.timeline()
+	.from('#elem2', { y: '100%', opacity: 0, duration: 1, ease: 'sine.out' })
+	.to('.stage__wrap', { y: -350, duration: 1, ease: 'sine.out' }, '>-15%')
+	.to('.stage__wrap', { y: -1400, duration: 3, ease: 'sine.out' })
+	.to('.stage__wrap', { y: -2500, duration: 4, ease: 'sine.out' })
+	.to('.stage__bg', { height: '100vh', borderRadius: 0, duration: 1 }, '<')
+// .to('#elem2', { y: 0, opacity: 1, duration: 2, ease: 'none' })
+// .to('#elem1', { y: '-100%', opacity: 0, duration: 2, ease: 'none' }, '<+30%')
+
+ScrollTrigger.create({
+	trigger: '.stage',
+	animation: stageTl,
+	start: '0% 0%',
+	end: '100% 0%',
+	scrub: 2,
+	pin: 'body',
+})
---
+++ src/pages/index/index-entry.js
@@ -0,0 +1,17 @@
+require("./../../global/js/global-entry")
+
+import "./index.scss"
+import gsap from "gsap"
+
+/**
+ * outImageUrl - change image url in svg (if image save with link)
+ * needContent - return content to js file
+ * par - preserveAspectRatio attribute for svg
+ * class - class for svg <svg class=""></svg>
+ * ./img/test_svg/test.svg - path to svg in src
+ */
+let svgContent = require("!!svg-anim-loader?outImageUrl=./assets/img/&needContent=false&par=''&class=''!./img/elem1.svg")
+require("!!svg-anim-loader?outImageUrl=./assets/img/&needContent=false&par=''&class=''!./img/elem2.svg")
+// console.log(svgContent);
+
+require('./app')
